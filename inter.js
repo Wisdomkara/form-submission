3d0e7c360632f957986caef7290868507e7327de
Wisdom Kara <wiskara1@gmail.com>
2024-04-09 00:56:16 +0100

    form

--- inter.js
+++ inter.js
@@ -1,18 +1,19 @@
 // first form submission
 
-const form = document.querySelector(".btn");
+let form = document.querySelector("#btn");
 const out = document.querySelector(".out");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   out.textContent = `Congratulations you have submitted your data for proccessing`;
   out.classList.add("design");
+  const Fullname = document.getElementById("Fullname").value;
+  const E_mail = document.getElementById("Email").value;
+  const date_of_birth = document.getElementById("date_of_birth").value;
+  const password = document.getElementById("password").value;
+  localStorage.setItem("Full name", Fullname);
+  localStorage.setItem("Email", E_mail);
+  localStorage.setItem("date of birth", date_of_birth);
+  localStorage.setItem("password", password);
 });
-
-// const form = document.querySelector(".btn");
-// const out = document.querySelector(".output");
-
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   out.textContent = `congratulations you have successfully won`;
-// });
+console.log(form);
